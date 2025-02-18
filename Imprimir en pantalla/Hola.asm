cfc463a671f2cce61edef3c2cc5db2d377b89fb8
Nmaurii <144194768+Nmaurii@users.noreply.github.com>
2025-02-18 01:19:02 -0300

    Create Hola.asm

---
+++ Imprimir en pantalla/Hola.asm
@@ -0,0 +1,20 @@
+section .data
+    msg db "Hola !",0xA;
+    len equ $ - msg;
+
+section .text
+    global _start;
+
+_start:
+
+    mov rax,1;
+    mov rdi,1;
+    mov rsi,msg;
+    mov rdx,len;
+    syscall;
+
+
+    mov rax,60;
+    xor rdi,rdi;
+    syscall
+
