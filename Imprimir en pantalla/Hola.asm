f2ed54ce0baa92d44c7cd319e9d67d5ad264d305
Nmaurii <144194768+Nmaurii@users.noreply.github.com>
2025-02-18 01:33:46 -0300

    Update Hola.asm

--- Imprimir en pantalla/Hola.asm
+++ Imprimir en pantalla/Hola.asm
@@ -1,20 +1,21 @@
 section .data
-    msg db "Hola !",0xA;
-    len equ $ - msg;
+    msg db "Hola !",0xA ;mensaje mas salto de linea en hexadecimal
+    len equ $ - msg     ;largo del mensaje considerando salto de linea (automatico)
 
 section .text
-    global _start;
+    global _start
 
 _start:
 
-    mov rax,1;
-    mov rdi,1;
-    mov rsi,msg;
-    mov rdx,len;
-    syscall;
+    ;llamada al sistema para imprimir en pantalla, se usan registros para alamacenar la id de la syscall
+    mov rax,1
+    mov rdi,1
+    mov rsi,msg
+    mov rdx,len
+    syscall
 
-
-    mov rax,60;
-    xor rdi,rdi;
+    ;llamada al sistena para terminar la ejecuacion
+    mov rax,60
+    xor rdi,rdi
     syscall
 
